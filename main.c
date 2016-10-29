891966e6c9ec40c9e1030d52641d366ec286cbc1
mm1994uestc <547238541@qq.com>
2016-10-29 20:51:41 +0800

    main

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+
+static int MyPow(int Num,int N);
 int main(void)
 {
-	printf("Hello world!");
+	int Number;
+	printf("Please enter a new number for calculating:");
+	scanf("%d",&Number);
+	printf("The result we calculate is equal to:%d\n",MyPow(Number,3));
 	return 0;
+}
+static int MyPow(int Num,int N)
+{
+	int i=0,Res=1;
+	for(i=0;i<N;i++)
+	{
+		Res*=Num;
+	}
+	return Res;
 }
