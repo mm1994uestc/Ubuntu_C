d880727fef73c2e6b21b221a5d634f2696e9c525
mm1994uestc <547238541@qq.com>
2016-10-31 20:23:59 +0800

    -A

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 static int MyPow(int Num,int N);
+static void MyMoto(void);
 int main(void)
 {
 	int Number;
 	printf("Please enter a new number for calculating:");
 	scanf("%d",&Number);
 	printf("The result we calculate is equal to:%d\n",MyPow(Number,3));
+	MyMoto();
 	return 0;
 }
 static int MyPow(int Num,int N)
@@ -17,4 +19,8 @@ static int MyPow(int Num,int N)
 		Res*=Num;
 	}
 	return Res;
+}
+static void MyMoto(void)
+{
+	printf("If you don't know what you should do,Just come to Ubuntu-Operation-System for Playing!\n");
 }
